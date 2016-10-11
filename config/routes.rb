dd842ea7d31d59144bf2c07f925f321597f024d1
Muneeba Khatoon <khatoon.muneeba@gmail.com>
2016-10-11 16:31:03 +0000

    fin toy_app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+  
+  get 'static_pages/goofy'
+
   resources :users
   resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'users#index'
+root 'static_pages#home'
 
 end
